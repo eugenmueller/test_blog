c5d88f3bf287a26cc521e37fd64b5ce95b5b7f5f
Eugen <eugen.mllr@gmail.com>
2022-01-27 15:30:44 +0100

    Add root path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root 'articles#index'
+
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
